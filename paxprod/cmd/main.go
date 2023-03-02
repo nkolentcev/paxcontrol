9a2a00237239ee1ef70c64d160d1d8730bd374f0
Nikolay Kolentcev <nkolentcev@gmail.com>
2023-03-02 20:03:20 +0300

    fix repo

--- paxprod/cmd/main.go
+++ paxprod/cmd/main.go
@@ -6,7 +6,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, BBB!")
+		return c.SendString("Hello, MIRRRR")
 	})
 
 	app.Listen(":8000")
