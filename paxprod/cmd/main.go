a2c7ddd80d1d2a380e068b08d3180621759e21ab
Nikolay Kolentcev <NKolentcev@gmail.com>
2023-03-03 14:41:09 +0300

    fiber mw logger

--- paxprod/cmd/main.go
+++ paxprod/cmd/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/nkolentcev/paxcontrol/paxprod/database"
 )
 
 func main() {
 	database.ConnectDB()
 	app := fiber.New()
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+	}))
 	setupRoutes(app)
 
 	app.Listen(":8000")
